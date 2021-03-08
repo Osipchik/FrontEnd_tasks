30f24595d3d7222d4ee9917518b50d00c41deb85
Tim-Osipchik <osipchiktim@gmail.com>
2021-03-08 23:04:57 +0300

    added history sending on connect

--- chat/app.js
+++ chat/app.js
@@ -1,14 +1,7 @@
-const dgram = require('dgram');
 const readline = require('readline');
 const crypto = require('crypto');
+const Client = require('./client');
 
-
-const Actions = {
-    message: 'message',
-    connect: 'connect',
-    check: 'check',
-    remove: 'remove'
-};
 
 const state = {
     currentUser: '',
@@ -16,8 +9,6 @@ const state = {
     companionPort: 0,
     companionName: '',
     currentHistory: null,
-    messenger: false,
-    client: null
 };
 
 const rl = readline.createInterface({
@@ -25,6 +16,46 @@ const rl = readline.createInterface({
     output: process.stdout,
     prompt: '>> '
 });
+
+
+
+Client.on('message', (rinfo, sender, payload) => {
+    const [message, owner] = [...payload.split(':')]
+
+    state.history[generateHash([state.currentUser, sender])].push([owner, message]);
+    if (state.companionName === sender) {
+        rl.print(`${owner}: ${message}`);
+    }
+})
+
+Client.on('check', (rinfo, sender, payload) => {
+    Client.sendTo(rinfo.port, 'connect', state.currentUser === sender)
+    if (state.currentUser === sender) {
+        ensureCreateProperty(state.history, generateHash([state.currentUser, payload]), [])
+
+        let hash = generateHash([state.currentUser, state.companionName]);
+        const history = state.history[hash]
+        if (history) {
+            history.forEach(i => { Client.sendTo(rinfo.port, 'message', state.currentUser, `${i[1]}:${i[0]}`) })
+        }
+    }
+})
+
+Client.on('connect', (rinfo, sender, payload) => {
+    if (sender === 'true') {
+        let hash = generateHash([state.currentUser, state.companionName]);
+        ensureCreateProperty(state.history, hash, []);        
+        state.companionPort = rinfo.port;
+        rl.print('---connected---')
+    }
+    else {
+        rl.print('---connection failed---')
+    }
+
+    rl.prompt();
+})
+
+
 
 rl.print = (line) => {
     process.stdout.clearLine();
@@ -44,46 +75,24 @@ rl.on('line', (line) => {
             state.currentUser = '';
             state.companionName = '';
             state.companionPort = 0;
-            state.messenger = false;
             state.client.close();
             break;
         case 'disconnect':
             state.companionName = '';
             state.companionPort = 0;
-            state.messenger = false;
             break;
         case 'connect':
             connectToUser();
             break;
-        case 'messenger':
-            if (state.companionPort) {
-                state.messenger = true;
-                let hash = generateHash([state.currentUser, state.companionName]);
-                state.history[hash].forEach(i => {
-                    if (i[0] === state.currentUser) {
-                        rl.prompt();
-                        console.log(i[1]);
-                    }
-                    else {
-                        console.log(`${i[0]}: ${i[1]}`);
-                    }
-                })
-            }
-            break;
         default:
-            if (state.messenger) {
-                let hash = generateHash([state.currentUser, state.companionName]);
-                state.history[hash].push([state.currentUser, line]);
-                sendToClient(state.companionPort, Actions.message, state.currentUser, line);
-            }
-            else {
-                console.log(`Say what? I might have heard '${line.trim()}'`);
-            }
+            let hash = generateHash([state.currentUser, state.companionName]);
+            state.history[hash].push([state.currentUser, line]);
+            Client.sendTo(state.companionPort, 'message', state.currentUser, `${line}:${state.currentUser}`)
             break;
     }
     rl.prompt();
 }).on('close', () => {
-    state.client.close();
+    Client.onClose()
     process.exit(0);
 });
 
@@ -93,7 +102,7 @@ function login() {
     rl.question('userName: ', (name) => {
         rl.question('port: ', (port) => {
             state.currentUser = name;
-            state.client = createClient(port);
+            Client.listen(port)
             rl.prompt();
         })
     })
@@ -103,53 +112,10 @@ function connectToUser() {
     rl.question('name: ', (name) => {
         rl.question('port: ', (port) => {
             state.companionName = name;
-            sendToClient(port, Actions.check, name, state.currentUser);
+            Client.sendTo(port, 'check', name, state.currentUser)
             rl.prompt();
         })
     })
-}
-
-
-
-function sendToClient(port, ...args) {
-    state.client.send(Buffer.from(args.join('/')), port, 'localhost');
-}
-
-
-function createClient(port) {
-    let client = dgram.createSocket('udp4');
-    client.on('message', (msg, rinfo) => {
-        let data = msg.toString().split('/');
-
-        if (data[0] === Actions.message && state.currentUser) {
-            state.history[generateHash([state.currentUser, data[1]])].push([data[1], data[2]]);
-            if (state.companionName === data[1]) {
-                rl.print(`${data[1]}: ${data[2]}`);
-            }
-        }
-        else if (data[0] === Actions.check) {
-            if (state.currentUser === data[1]) {
-                ensureCreateProperty(state.history, generateHash([state.currentUser, data[2]]), [])
-            }
-            sendToClient(rinfo.port, Actions.connect, state.currentUser === data[1]);
-        }
-        else if (data[0] === Actions.connect) {
-            if (data[1] === 'true') {
-                let hash = generateHash([state.currentUser, state.companionName]);
-                ensureCreateProperty(state.history, hash, []);
-                state.companionPort = rinfo.port;
-                rl.print('connected')
-            }
-            else {
-                rl.print('connection failed')
-            }
-            rl.prompt();
-        }
-    });
-
-    client.bind(parseInt(port), 'localhost');
-
-    return client;
 }
 
 
@@ -163,8 +129,8 @@ function ensureCreateProperty(obj, prop, create) {
 function generateHash(arr) {
     const hash = (str) => crypto.createHash('md5').update(str).digest("hex");
 
-    let strOne = hash(arr[0]);
-    let strTwo = hash(arr[1]);
+    const strOne = hash(arr[0]);
+    const strTwo = hash(arr[1]);
 
     let res = ''
 
